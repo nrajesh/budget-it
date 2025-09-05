0e16bf25a3def2c4d6872c941d9f733987916354
[dyad] <Narayanan.Rajesh@outlook.com>
2025-09-05 21:22:01 +0200

    Reverted all changes back to version 1d7c00ab894d8e12d14e799bed9b66753b262741

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { CurrencyProvider } from "./contexts/CurrencyContext.tsx";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // Import QueryClient and QueryClientProvider
-
-const queryClient = new QueryClient(); // Create a new QueryClient instance
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}> {/* Wrap with QueryClientProvider */}
-      <CurrencyProvider>
-        <App />
-      </CurrencyProvider>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <CurrencyProvider>
+    <App />
+  </CurrencyProvider>
 );
