dd4185135b407425b13c714c326e88b53d7c7d46
[dyad] <Narayanan.Rajesh@outlook.com>
2025-09-08 21:52:06 +0200

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { CurrencyProvider } from "./contexts/CurrencyContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
