840d9ac6921014cab87458f25b5150b8fa07a16f
[dyad] <Narayanan.Rajesh@outlook.com>
2025-09-08 21:54:16 +0200

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,27 @@ import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { CurrencyProvider } from "./contexts/CurrencyContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        storageKey="portfolio-theme"
+      >
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <FeatureToggleProvider>
+              <TooltipProvider>
+                <App />
+                <SpeedInsights />
+              </TooltipProvider>
+            </FeatureToggleProvider>
+          </AuthProvider>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+  
   <CurrencyProvider>
     <App />
   </CurrencyProvider>
