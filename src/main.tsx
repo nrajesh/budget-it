ad6df077b794bc0fca103370c6153cabae16a049
[dyad] <Narayanan.Rajesh@outlook.com>
2025-09-26 22:29:00 +0200

    [dyad] wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,24 @@ import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { CurrencyProvider } from "./contexts/CurrencyContext.tsx";
+import { UserProvider } from "./contexts/UserContext.tsx";
+import { TransactionsProvider } from "./contexts/TransactionsContext.tsx";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="dark" storageKey="vite-ui-theme">
-      <CurrencyProvider>
-        <App />
-      </CurrencyProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="dark" storageKey="vite-ui-theme">
+        <UserProvider>
+          <CurrencyProvider>
+            <TransactionsProvider>
+              <App />
+            </TransactionsProvider>
+          </CurrencyProvider>
+        </UserProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
