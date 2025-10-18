1e4d093167c2d143ac2ebec2f6c44832249ccda7
[dyad] <Narayanan.Rajesh@outlook.com>
2025-10-18 10:59:46 +0200

    [dyad] Fixed date range text overlap in filter bar - wrote 1 file(s)

--- src/components/ui/date-range-picker.tsx
+++ src/components/ui/date-range-picker.tsx
@@ -37,18 +37,20 @@ export function DatePickerWithRange({
             )}
           >
             <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
-                </>
+            <span className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap">
+              {date?.from ? (
+                date.to ? (
+                  <>
+                    {format(date.from, "LLL dd, y")} -{" "}
+                    {format(date.to, "LLL dd, y")}
+                  </>
+                ) : (
+                  format(date.from, "LLL dd, y")
+                )
               ) : (
-                format(date.from, "LLL dd, y")
-              )
-            ) : (
-              <span>Pick a date</span>
-            )}
+                <span>Pick a date</span>
+              )}
+            </span>
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0" align="start">
